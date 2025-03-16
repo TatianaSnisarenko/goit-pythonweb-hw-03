ddd1f6d22c230070f1237a7077b8a2feb7de9a1f
Tatiana Snisarenko <tatiana.snisarenko.p@gmail.com>
2025-03-16 09:37:00 +0200

    Add Homework three

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # goit-pythonweb-hw-03
+
+## Project Description
+
+This project is a simple web application built with Python. It has three main pages:
+
+- **Home Page**: Describes the available Python training packages.
+- **Send Message**: Allows users to send a message with their name and message content.
+- **Read Messages**: Displays the messages that have been sent.
+
+## Running the Project in a Docker Container
+
+Build the image using command:
+
+```sh
+docker build --no-cache . -t hw3
+```
+
+To run the project in a Docker container, use the following command:
+
+```sh
+docker run -d -p 3000:3000 -v ./storage:/app/storage --name hw3c hw3
+```
---
+++ main.py
@@ -0,0 +1,111 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import urllib.parse
+import mimetypes
+import pathlib
+import os
+import json
+from datetime import datetime
+from jinja2 import Environment, FileSystemLoader
+
+STORAGE_DIR = "storage"
+DATA_FILE = os.path.join(STORAGE_DIR, "data.json")
+
+
+class HttpHandler(BaseHTTPRequestHandler):
+
+    def do_POST(self):
+        data = self.rfile.read(int(self.headers["Content-Length"]))
+        data_parse = urllib.parse.unquote_plus(data.decode())
+        data_dict = {
+            key: value for key, value in [el.split("=") for el in data_parse.split("&")]
+        }
+        self.store_message(data_dict)
+
+        self.send_response(302)
+        self.send_header("Location", "/")
+        self.end_headers()
+
+    def do_GET(self):
+        pr_url = urllib.parse.urlparse(self.path)
+        if pr_url.path == "/":
+            self.send_html_file("index.html")
+        elif pr_url.path == "/message":
+            self.send_html_file("message.html")
+        elif pr_url.path == "/read":
+            self.show_messages()
+        else:
+            if pathlib.Path().joinpath(pr_url.path[1:]).exists():
+                self.send_static()
+            else:
+                self.send_html_file("error.html", 404)
+
+    def store_message(self, data_dict):
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
+        if not os.path.exists(STORAGE_DIR):
+            os.makedirs(STORAGE_DIR)
+
+        if os.path.exists(DATA_FILE):
+            with open(DATA_FILE, "r", encoding="utf-8") as file:
+                try:
+                    data_store = json.load(file)
+                except json.JSONDecodeError:
+                    data_store = {}
+        else:
+            data_store = {}
+
+        data_store[timestamp] = data_dict
+
+        with open(DATA_FILE, "w", encoding="utf-8") as file:
+            json.dump(data_store, file, indent=2, ensure_ascii=False)
+
+    def show_messages(self):
+        if os.path.exists(DATA_FILE):
+            with open(DATA_FILE, "r", encoding="utf-8") as file:
+                try:
+                    messages = json.load(file)
+                except json.JSONDecodeError:
+                    messages = {}
+        else:
+            messages = {}
+
+        env = Environment(loader=FileSystemLoader("."))
+        template = env.get_template("messages_template.html")
+
+        html_content = template.render(
+            messages=messages,
+        )
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(html_content.encode("utf-8"))
+
+    def send_html_file(self, filename, status=200):
+        self.send_response(status)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        with open(filename, "rb") as fd:
+            self.wfile.write(fd.read())
+
+    def send_static(self):
+        self.send_response(200)
+        mt = mimetypes.guess_type(self.path)
+        if mt:
+            self.send_header("Content-type", mt[0])
+        else:
+            self.send_header("Content-type", "text/plain")
+        self.end_headers()
+        with open(f".{self.path}", "rb") as file:
+            self.wfile.write(file.read())
+
+
+def run(server_class=HTTPServer, handler_class=HttpHandler):
+    server_address = ("", 3000)
+    http = server_class(server_address, handler_class)
+    try:
+        http.serve_forever()
+    except KeyboardInterrupt:
+        http.server_close()
+
+
+if __name__ == "__main__":
+    run()
